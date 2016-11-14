b01e457863dcc468a4635db6b972a720f4247fb9
Edwin Carrasquillo <ecarra7979@gmail.com>
2016-11-13 23:47:58 -0500

    adding list code for list app

---
+++ main.py
@@ -0,0 +1,47 @@
+#Initialize list
+shopping_list = []
+
+def show_help():
+    #Give Guidence
+    print("What are you getting?: ")
+    print("""
+Enter 'HELP' to show this help.
+Enter 'SHOW' to see current list.
+Enter 'DONE' to exit list.
+Enter 'DELETE' to remove item from list.
+    """)
+
+show_help()
+
+def show_list():
+    print("Here is your list.")
+    #show contents of list
+    for item in shopping_list:
+        print(item)
+def add_to_list(new_item):
+    #add to list
+    shopping_list.append(new_item)
+    print("Added {}. You have {} items in list.".format(new_item, len(shopping_list)))
+def delete_item(del_item):
+    #remove item from list
+    shopping_list.remove(del_item)
+
+#loop until False
+while True:
+    #grab input
+    new_item = input("> ")
+    #Check to see if equal to DONE to exit app
+    if new_item == "DONE":
+        break
+    elif new_item == "HELP":
+        show_help()
+        continue
+    elif new_item == "SHOW":
+        show_list()
+        continue
+    elif new_item == "DELETE":
+        del_item = input("What item would you liked removed? ")
+        delete_item(del_item)
+        continue
+    add_to_list(new_item)
+show_list()    
