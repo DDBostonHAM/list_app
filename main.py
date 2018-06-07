d56ad9f08d6f0de5cf57a5b3deb60d4836f105c4
Edwin Carrasquillo <edwin.carrasquillo@datadoghq.com>
2018-06-07 12:53:17 -0400

    update/fix

--- main.py
+++ main.py
@@ -1,47 +1,53 @@
+import os
+
 #Initialize list
 shopping_list = []
 
+
 def show_help():
     #Give Guidence
-    print("What are you getting?: ")
-    print("""
-Enter 'HELP' to show this help.
-Enter 'SHOW' to see current list.
-Enter 'DONE' to exit list.
-Enter 'DELETE' to remove item from list.
-    """)
-
-show_help()
+    print("Available Commands: ")
+    print(" \
+    Enter 'HELP' to show this help.\n \
+    Enter 'SHOW' to see current list.\n \
+    Enter 'DONE' to exit list.\n \
+    Enter 'DELETE' to remove item from list.")
 
 def show_list():
-    print("Here is your list.")
+    print("List:")
     #show contents of list
     for item in shopping_list:
         print(item)
-def add_to_list(new_item):
+def add_to_list(entry):
     #add to list
-    shopping_list.append(new_item)
-    print("Added {}. You have {} items in list.".format(new_item, len(shopping_list)))
+    shopping_list.append(entry)
+    print("Added {}. You have {} items in list.".format(entry, len(shopping_list)))
 def delete_item(del_item):
     #remove item from list
     shopping_list.remove(del_item)
 
+def cleaning():
+    os.system('clear')
+
+show_help()
 #loop until False
 while True:
-    #grab input
-    new_item = input("> ")
-    #Check to see if equal to DONE to exit app
-    if new_item == "DONE" or new_item == "done":
+    entry = input("Add Item >:")
+    cleaning()
+
+    if entry == "done":
         break
-    elif new_item == "HELP" or new_item == "help":
+    elif entry == "help":
         show_help()
         continue
-    elif new_item == "SHOW" or new_item == "show":
+    elif entry == "show":
         show_list()
         continue
-    elif new_item == "DELETE" or new_item == "delete":
+    elif entry == "delete":
+        show_list()
         del_item = input("What item would you liked removed? ")
         delete_item(del_item)
+        show_list()
         continue
-    add_to_list(new_item)
-show_list()    
+    add_to_list(entry)
+show_list()
